3630bacc7c710ab1ad78dbfb8c6d4c164ae92f81
79mir <92146579+79mir@users.noreply.github.com>
2021-11-25 14:02:13 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,10 +1,17 @@
-function mz_0price_pre_get_posts_query( $query ) {
- if( $query->is_page( array( 'regulamin', 'moje-konto', 'kontakt', 'koszyk') ) ) return $query;
+function mz_price_stock_0_pre_get_posts_query( $query ) {
+ if( $query->is_admin || $query->is_page( array( 'start', 'moje-konto', 'kontakt', 'cookie', 'regulamin', 'wysyłka', 'koszyk', 'kasa') ) ) return $query;
  $query->set( 'meta_query', array(array(
+	'key'       => '_stock',
+	'value'     => '0',
+	'compare'   => 'NOT IN' ),
+	array(
+	'key'       => '_stock_status',
+	'value'     => 'outofstock',
+	'compare'   => 'NOT IN' ),							  
+	array(
 	'key'       => '_price',
 	'value'     => '0',
-	'compare'   => 'NOT IN')
+	'compare'   => 'NOT IN'
  )));
  return $query;
 }
-add_action( 'pre_get_posts', 'mz_0price_pre_get_posts_query', 100 );
